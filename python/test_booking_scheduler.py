d24f902f1c0c1fc9a06ad8dd8d411bd403ed41ed
jaewon0.min <jaewon0.min@gmail.com>
2025-08-01 13:01:58 +0900

    step8

--- python/test_booking_scheduler.py
+++ python/test_booking_scheduler.py
@@ -1,6 +1,6 @@
 import pytest
 
-from python.test_communication import TestableSmsSender
+from python.test_communication import TestableSmsSender, TestableMailSender
 from schedule import Customer, Schedule
 from communication import SmsSender, MailSender
 from booking_scheduler import BookingScheduler
@@ -65,8 +65,13 @@ def test_예약완료시_SMS는_무조건_발송(booking_scheduler_with_sms_mock
     booking_scheduler.add_schedule(schedule)
     assert sms_mock.send_called
 
-def test_이메일이_없는_경우에는_이메일_미발송():
-    pass
+def test_이메일이_없는_경우에는_이메일_미발송(booking_scheduler):
+    mail_sender = TestableMailSender()
+    booking_scheduler.set_mail_sender(mail_sender)
+    schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
+    booking_scheduler.add_schedule(schedule)
+
+    assert mail_sender.send_mail_count == 0
 
 def test_이메일이_있는_경우에는_이메일_발송():
     pass
